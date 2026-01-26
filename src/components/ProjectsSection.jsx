9b6ca7a0143e7007ad5ea296d2334c69c51f63e3
unknown <Razzak118348>
2026-01-27 01:59:19 +0600

    grid change of project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -74,7 +74,7 @@ export const ProjectsSection = () => {
         </p>
 
         {/* Featured Projects */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project, i) => (
             <motion.div
               key={project.id}
